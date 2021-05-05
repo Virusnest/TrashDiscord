58a2b71b3ceeec4f1be4ac382928aa26364e7943
Virusnest <kai.maloney.au@gmail.com>
2021-05-05 00:05:37 +0000

    updated auth location

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
   apiKey: "AIzaSyBlCmXMP1hdbZIibDAhENHb__uLC4VrEtw",
-    authDomain: "trashdiscord-70a03.firebaseapp.com",
+    authDomain: "trash-discord.herokuapp.com",
     projectId: "trashdiscord-70a03",
     storageBucket: "trashdiscord-70a03.appspot.com",
     messagingSenderId: "955465757058",
